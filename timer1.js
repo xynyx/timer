2bb622ce9a41ecbfa9431809f1dc01332cc5bfc8
Matt Taylor <matt.taylr22@gmail.com>
2020-03-10 16:10:13 +0000

    timer with @kelsey-griffin

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const input = process.argv.slice(2);
+
+input.forEach(el => {
+  if (el < 0 || isNaN(el)) {
+    return;
+  }
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, el * 1000);
+
+});
